05c0b02337171fe1de5b602b3d80d1ab718d6114
b1 <alch3mist94@protonmail.com>
2021-01-04 01:06:22 +0100

    switched to external script and eventlistening for dynamic elements

--- README.md
+++ README.md
@@ -13,41 +13,13 @@ Easy-to-use embeddable and customizable html tooltip which allows the display of
 A little script tag in your html and an `icon` or `img` is everything you need!
 Place the following script in the header or source it of and adjust the global parameters to your demands. `Note:`If typing is enabled, the typing speed and randomness (human-like) of the typing is mimicked.
 ```html
-<script>
-    window.addEventListener('load', function () {
-        // ---- globals ----
-        const iconPath = "./tooltip.ico";
-        const color = "red";
-        const bordC = "red"; // border color
-        const hoverBackground = "rgb(0,0,0,0.5)";
-        const fontSize = 12; //px
-        const radius = 5; //px
-        const typing = false; 
-        const typingSpeed=1; // only if typing is true
-        const typingLikeHuman=false;
-        // -----------------
-        function el(type){return document.createElement(type)}var tags=Array.prototype.slice.call(
-        document.getElementsByTagName("tooltip"));tags.forEach(tag=>{var txt=tag.innerHTML;tag//20
-        .innerHTML="";tag.style.display="block";tag.style.overflow="hidden";var wrap=el('div');var
-        ico=el('img');ico.style.position="relative";ico.src=iconPath;wrap.style.height="90%";wrap.
-        style.width="90%";wrap.style.padding="5%";wrap.style.position="relative";ico.style.display
-        ="block";ico.style.height="100%";ico.style.width="100%";function delay(seconds){return new
-        Promise(function(resolve){setTimeout(function(){resolve(00)},1000*seconds)});}var info=el(
-        'span');tag.appendChild(info);async function hoverOrTouchEvent(e){breaker=false;info.style
-        .backgroundColor=hoverBackground;info.style.hypens="auto";info.style.maxWidth="300px";info
-        .style.borderRadius=`${radius}px`;info.style.zIndex="1000";info.style.position="absolute";
-        info.style.border=`1px solid ${bordC}`;info.style.left=`${e.pageX}px`;info.style.top=`${e.
-        pageY}px`;info.id="tooltip-info-text";info.style.color=color;info.style.fontSize=fontSize;
-        info.style.padding="20px";info.style.transition="0.40s";info.style.opacity="1";info.style.
-        maxHeight="auto";info.style.overflow="hidden";const t=[];for(let i=0;i<50;i++){t.push(Math
-        .random())};if(typing){info.innerHTML="";for(var i=0;i<txt.length;i++){info.innerHTML+=txt
-        .charAt(i);if(typingLikeHuman){await delay((.03+.2*t[i%49])/typingSpeed)}else{await delay(
-        .07/typingSpeed)}if(breaker){break}}}else{info.innerHTML=txt}};ico.onmouseover=function(e)
-        {hoverOrTouchEvent(e)};ico.onmouseout=function(e){info.style.opacity="0";breaker=true};ico
-        .ontouchstart=function(e){hoverOrTouchEvent(e)};wrap.appendChild(ico);tag.appendChild(wrap
-        );});/*buffer_for_fixes################################# https://github.com/B0-B © 2020 */
-    });
-</script>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <script type="text/javascript" src="./tooltip.js"></script>
+        <script>toolTipHandler(window)</script>
+    </head>
+    ...
 ```
 ## Usage
 Set the `tooltip` tag to a prefered place of your page
--- tooltip.js
+++ tooltip.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+var toolTipHandler = async function () {
     // ---- globals ----
     const iconPath = "./tooltip.ico";
     const color = "white";
@@ -10,66 +10,83 @@ window.addEventListener('load', function () {
     const typingLikeHuman=true;
     // -----------------
 
+    console.log('run rooltip listener')
 
     function el(type){return document.createElement(type)}
-    var tags=Array.prototype.slice.call(document.getElementsByTagName("tooltip"));
-    tags.forEach(tag => {
-        // ---- reshape parent ----
-        var txt=tag.innerHTML;tag.innerHTML="";
-        tag.style.display="block";tag.style.overflow="hidden";
+    function delay(seconds){return new Promise(function(resolve){setTimeout(function(){resolve(0)}, 1000*seconds)});}
 
-        // ---- icon ----
-        var wrap = el('div');wrap.style.height="90%";wrap.style.width="90%";
-        wrap.style.padding="5%";wrap.style.position="relative";
-        var ico = el('img');ico.style.position="relative";ico.src=iconPath;
-        ico.style.display="block";ico.style.height="100%";ico.style.width="100%";
+    while (true) {
+        var tags=Array.prototype.slice.call(document.getElementsByTagName("tooltip"));
+        tags.forEach(tag => {
+            // ---- reshape parent ----
+            const attributes = tag.attributes;
+            var txt=tag.innerHTML;
+            const grandParentNode = tag.parentNode;
+            grandParentNode.removeChild(tag);
+            newTag = el('tt'); grandParentNode.appendChild(newTag);
 
-        // ---- info ----
-        var info=el('span');
-        tag.appendChild(info);
-        var breaker=false;
-        function delay(seconds){return new Promise(function(resolve){setTimeout(function(){resolve(0)}, 1000*seconds)});}
-        async function hoverOrTouchEvent(e) {
-            console.log(e.pageX, e.pageY)
-            breaker=false;
-            info.style.backgroundColor=hoverBackground;info.style.hypens="auto";
-            info.style.zIndex="100";info.style.maxWidth="300px"; info.style.borderRadius=`${radius}px`;
-            info.style.border=`1px solid ${color}`;info.style.position="absolute"; info.style.left=`${e.pageX}px`;
-            info.style.top=`${e.pageY}px`;info.id="tooltip-info-text";
-            info.style.color=color; info.style.padding="20px";info.style.fontSize=fontSize;
-            info.style.transition="0.4s"; info.style.opacity="1";
-            info.style.maxHeight="1000px"; info.style.overflow="hidden";
-            var t=[];for(let i = 0; i < 50; i++) {t.push(Math.random());};
-            if (typing) {
-                info.innerHTML="";
-                for (var i = 0; i < txt.length; i++) {
-                    info.innerHTML+=txt.charAt(i);
-                    if (typingLikeHuman){
-                        await delay((.03+.2*t[i % 49])/typingSpeed);
-                    } else {
-                        await delay(.07/typingSpeed);
+            // loop through oldScript attributes and apply them on new script
+            console.log('attrubutes', attributes);
+            newTag.style.width = tag.style.width;
+            newTag.style.height = tag.style.height;
+            tag = newTag;
+            //Object.keys(attributes).forEach(k => {console.log(k, attributes[k]); tag.setAttribute(k, attributes[k])});
+            
+            tag.style.display="block";tag.style.overflow="hidden";
+
+            // ---- icon ----
+            var wrap = el('div');wrap.style.height="90%";wrap.style.width="90%";
+            wrap.style.padding="5%";wrap.style.position="relative";
+            var ico = el('img');ico.style.position="relative";ico.src=iconPath;
+            ico.style.display="block";ico.style.height="100%";ico.style.width="100%";
+
+            // ---- info ----
+            var info=el('span');
+            tag.appendChild(info);
+            var breaker=false;
+            async function hoverOrTouchEvent(e) {
+                console.log(e.pageX, e.pageY)
+                breaker=false;
+                info.style.backgroundColor=hoverBackground;info.style.hypens="auto";
+                info.style.zIndex="100";info.style.maxWidth="300px"; info.style.borderRadius=`${radius}px`;
+                info.style.border=`1px solid ${color}`;info.style.position="absolute"; info.style.left=`${e.pageX}px`;
+                info.style.top=`${e.pageY}px`;info.id="tooltip-info-text";
+                info.style.color=color; info.style.padding="20px";info.style.fontSize=fontSize;
+                info.style.transition="0.4s"; info.style.opacity="1";
+                info.style.maxHeight="1000px"; info.style.overflow="hidden";
+                var t=[];for(let i = 0; i < 50; i++) {t.push(Math.random());};
+                if (typing) {
+                    info.innerHTML="";
+                    for (var i = 0; i < txt.length; i++) {
+                        info.innerHTML+=txt.charAt(i);
+                        if (typingLikeHuman){
+                            await delay((.03+.2*t[i % 49])/typingSpeed);
+                        } else {
+                            await delay(.07/typingSpeed);
+                        }
+                        
+                        if (breaker) {break}
                     }
-                    
-                    if (breaker) {break}
+                } else {
+                    info.innerHTML=txt;
                 }
-            } else {
-                info.innerHTML=txt;
+            };
+            // mouse down for desktop or notebook clients
+            ico.onmouseover=function(e){
+                hoverOrTouchEvent(e);
+            };ico.onmouseout=function(e){
+                info.style.opacity="0";
+                breaker=true;
+                //tag.removeChild(info);
+            };ico.ontouchstart=function(e){
+                hoverOrTouchEvent(e);
             }
-        };
-        // mouse down for desktop or notebook clients
-        ico.onmouseover=function(e){
-            hoverOrTouchEvent(e);
-        };ico.onmouseout=function(e){
-            info.style.opacity="0";
-            breaker=true;
-            //tag.removeChild(info);
-        };ico.ontouchstart=function(e){
-            hoverOrTouchEvent(e);
-        }
-        // 
+            // 
 
-        // ---- add to document ----
-        wrap.appendChild(ico);
-        tag.appendChild(wrap);
-    });
-});
+            // ---- add to document ----
+            wrap.appendChild(ico);
+            tag.appendChild(wrap);
+        });
+        await delay(.05);
+    };
+}
